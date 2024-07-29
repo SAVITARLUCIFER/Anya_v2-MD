669510176fe264261eee5c84d29670670aace3c2
SAVITARLUCIFER <88197679+SAVITARLUCIFER@users.noreply.github.com>
2024-07-29 21:12:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,23 +5,23 @@ module.exports = {
     /**
      * bot details and parameters
      */
-    botname: process.env.BotName || "Queen Anya Bot",
-    footer: process.env.Footer || "© Queen Anya Bot",
+    botname: process.env.BotName || "LUCIFE_BOT",
+    footer: process.env.Footer || "LUCIFER",
     prefa: process.env.Prefix || "-",
     themeemoji: process.env.ThemeEmoji || "🎐",
 
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.Owner_Name || "ARAKKAL LUCIFER",
+    ownernumber: process.env.Owner_Number || "+917356466085",
+    instagramId: process.env.Insta || "_ARAKKAL_LUCIFER_",
 
     /**
      * other details and parameters
      */
-    author: process.env.Author || "@PikaBotz",
-    packname: process.env.PackName || "Queen Anya v2 MD",
+    author: process.env.Author || "LUCIFER",
+    packname: process.env.PackName || "LUCIFER",
     socialLink: process.env.Web || "https://github.com/PikaBotz",
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
